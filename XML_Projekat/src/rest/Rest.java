cc3e458cccb8d560360c8ec3583df0c2e3b22328
markotot <tot.marko@hotmail.com>
2016-06-09 15:48:26 +0200

    Odradjen setup za rest

---
+++ XML_Projekat/src/rest/Rest.java
@@ -0,0 +1,21 @@
+package rest;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Stateless
+@Path("/services")
+public class Rest {
+
+	@GET
+	@Path("/test")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String test(){
+		return "Test";
+	}
+	
+	
+}
