586c1d9d196a1f4db728884e811c9029036e7ae3
HighlanderSRB <nikola_stokic@yahoo.com>
2016-06-10 16:42:42 +0200

    Sredio izgled za predsednika vlade.

--- XML_Projekat/WebContent/angularControllers.js
+++ XML_Projekat/WebContent/angularControllers.js
@@ -1,5 +1,25 @@
+var app = angular.module('XML_App', []).controller(
+		'mainControler',
+		function($scope) {
+			$scope.sadrzajAkta = "";
 
-var app = angular.module('XML_App', []).controller('mainControler', function($scope) {
-    $scope.listaAkata = ["akt o oduzimanju radne dozvole", "akt2", "akt3"];
-    console.log($scope.listaAkata);
-});
+			$scope.listaAkata = [ "akt o oduzimanju radne dozvole", "akt2",
+					"akt3" ];
+			$scope.listaOdabranihAkata = [];
+
+			$scope.addAkt = function(name) {
+				var index = $scope.listaOdabranihAkata.indexOf(name);
+				if (index == -1) {
+					$scope.listaOdabranihAkata.push(name);
+				}
+			};
+
+			$scope.removeAkt = function(name) {
+				var index = $scope.listaOdabranihAkata.indexOf(name);
+				$scope.listaOdabranihAkata.splice(index, 1);
+			};
+
+			$scope.prikazSadrzajaAkta = function(id) {
+				$scope.sadrzajAkta = id;	
+			};
+		});
