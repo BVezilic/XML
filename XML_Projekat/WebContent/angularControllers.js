420c2d2fd7b61129f47894b9887c8e86682ab91d
HighlanderSRB <nikola_stokic@yahoo.com>
2016-06-11 15:23:30 +0200

    Dodato stokakvih sranja za frontend.

--- XML_Projekat/WebContent/angularControllers.js
+++ XML_Projekat/WebContent/angularControllers.js
@@ -1,25 +1,50 @@
-var app = angular.module('XML_App', []).controller(
-		'mainControler',
-		function($scope) {
-			$scope.sadrzajAkta = "";
+var app = angular
+		.module('XML_App', [ 'ngRoute' ])
+		.controller(
+				'mainControler',
+				function($scope) {
+					$scope.sadrzajAkta = "";
 
-			$scope.listaAkata = [ "akt o oduzimanju radne dozvole", "akt2",
-					"akt3" ];
-			$scope.listaOdabranihAkata = [];
+					$scope.listaAkata = [ "akt o oduzimanju radne dozvole",
+							"akt2", "akt3" ];
+					$scope.listaOdabranihAkata = [];
 
-			$scope.addAkt = function(name) {
-				var index = $scope.listaOdabranihAkata.indexOf(name);
-				if (index == -1) {
-					$scope.listaOdabranihAkata.push(name);
-				}
-			};
+					$scope.addAkt = function(name) {
+						var index = $scope.listaOdabranihAkata.indexOf(name);
+						if (index == -1) {
+							$scope.listaOdabranihAkata.push(name);
+						}
+					};
 
-			$scope.removeAkt = function(name) {
-				var index = $scope.listaOdabranihAkata.indexOf(name);
-				$scope.listaOdabranihAkata.splice(index, 1);
-			};
+					$scope.removeAkt = function(name) {
+						var index = $scope.listaOdabranihAkata.indexOf(name);
+						$scope.listaOdabranihAkata.splice(index, 1);
+					};
 
-			$scope.prikazSadrzajaAkta = function(id) {
-				$scope.sadrzajAkta = id;	
-			};
-		});
+					$scope.prikazSadrzajaAkta = function(id) {
+						$scope.sadrzajAkta = id;
+					};
+				})
+		.config(function($routeProvider) {
+			$routeProvider.when('/v1', {
+				templateUrl : 'odabirAkata.html'
+			}).when('/v2', {
+				templateUrl : 'kreiranjeAmandmana.html',
+				controller : 'amandmaniController'
+			});
+		})
+		.controller(
+				'amandmaniController',
+				function($scope) {
+					$scope.selectedAkt = "";
+					$scope.selectAkt = function(akt) {
+						$scope.selectedAkt = akt;
+
+					};
+					
+					$scope.test="";
+					
+					
+					
+
+				});
