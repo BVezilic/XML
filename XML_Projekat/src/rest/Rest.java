65eb69901ff0f460a11ee6ef1050b4d2fa488ce4
markotot <tot.marko@hotmail.com>
2016-06-13 17:15:12 +0200

    Login rest

--- XML_Projekat/src/rest/Rest.java
+++ XML_Projekat/src/rest/Rest.java
@@ -3,11 +3,13 @@ package rest;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import model.korisnik.Korisnici;
+import model.korisnik.Korisnik;
 
 @Stateless
 @Path("/services")
@@ -20,18 +22,44 @@ public class Rest {
 		return "Test";
 	}
 
-	@POST
-	@Path("/loginUser")
+	@GET
+	@Path("/login/{username}/{password}")
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.TEXT_PLAIN)
-	public Boolean loginUser(/* User u */) {
+	@Produces(MediaType.APPLICATION_XML)
+	public Korisnik loginUser(@PathParam("username") String username, @PathParam("password") String password) {
 
-		int temp = 1;
-		if (temp == 1/* pronadjes usera u bazi sa istim username i passwordom */) {
-			return true;
-		} else {
-			return false;
+		Korisnici korisnici = new Korisnici();
+		
+		Korisnik k1 = new Korisnik();
+		Korisnik k2 = new Korisnik();
+		Korisnik k3 = new Korisnik();
+		
+		k1.setKorisnickoIme("user1");
+		k2.setKorisnickoIme("user2");
+		k3.setKorisnickoIme("user3");
+
+		k1.setLozinka("pass1");
+		k2.setLozinka("pass2");
+		k3.setLozinka("pass3");
+		
+		k1.setUloga("obican");
+		k2.setUloga("specijalan");
+		k3.setUloga("najbolji");
+
+		
+		korisnici.getKorisnik().add(k1);
+		korisnici.getKorisnik().add(k2);
+		korisnici.getKorisnik().add(k3);
+		
+		
+		for (Korisnik  k : korisnici.getKorisnik()) {
+			if (k.getKorisnickoIme().equals(username) && k.getLozinka().equals(password)){
+				return k;
+			}
 		}
+		
+		return null;
+		
 	}
 
 	@GET
