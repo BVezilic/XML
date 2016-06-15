5b3d45f3508f89f658bcc710c8c612817692a7c7
HighlanderSRB <nikola_stokic@yahoo.com>
2016-06-15 14:57:32 +0200

    Odradjen font bez prekgleda dokumenata.

--- XML_Projekat/WebContent/angularControllers.js
+++ XML_Projekat/WebContent/angularControllers.js
@@ -1,4 +1,4 @@
-var user = "";
+var user = "gradjanin";
 var odabraniAmandmani = [];
 
 var app = angular
@@ -8,7 +8,30 @@ var app = angular
 				function($scope, $http, $window) {
 					$scope.user = "predsednikVlade";
 
-					//HTTP FUNKCIJE
+					$scope.listaIzglasanihAmandmana = [];
+					// HTTP FUNKCIJE
+					var loadAmandman = function() {
+						var req = {
+							method : 'GET',
+							url : 'http://localhost:8080/XML_Projekat/rest/services/amandmani'
+						}
+						$http(req).then(function successCallback(response) {
+							$scope.listaIzglasanihAmandmana = response.data;
+
+						});
+					};
+
+					var loadPredlozeniAmandman = function() {
+						var req = {
+							method : 'GET',
+							url : 'http://localhost:8080/XML_Projekat/rest/services/predlozeniAmandmani'
+						}
+						$http(req).then(function successCallback(response) {
+							$scope.listaAmandmana = response.data;
+
+						});
+					};
+
 					var loadAkt = function() {
 						var req = {
 							method : 'GET',
@@ -16,45 +39,47 @@ var app = angular
 						}
 						$http(req).then(function successCallback(response) {
 							$scope.listaAkata = response.data;
-							
+
 						});
 					};
-					
-					var loadOdabaniAkt = function(){
+
+					var loadOdabaniAkt = function() {
 						var req = {
-								method : 'GET',
-								url : 'http://localhost:8080/XML_Projekat/rest/services/odabraniAkti'
-							}
-							$http(req).then(function successCallback(response) {
-								$scope.listaOdabranihAkata = response.data;								
-							});
+							method : 'GET',
+							url : 'http://localhost:8080/XML_Projekat/rest/services/odabraniAkti'
+						}
+						$http(req).then(function successCallback(response) {
+							$scope.listaOdabranihAkata = response.data;
+						});
 					};
-					
-					var addAkt = function(naslov){
+
+					var addAkt = function(naslov) {
 						var req = {
-								method : 'PUT',
-								url : 'http://localhost:8080/XML_Projekat/rest/services/odabraniAkti/add/'+naslov
-							}
-							$http(req).then(function successCallback(response) {
-								$scope.listaOdabranihAkata = response.data;								
-							});
+							method : 'PUT',
+							url : 'http://localhost:8080/XML_Projekat/rest/services/odabraniAkti/add/'
+									+ naslov
+						}
+						$http(req).then(function successCallback(response) {
+							$scope.listaOdabranihAkata = response.data;
+						});
 					};
-					
-					var removeAkt = function(naslov){
+
+					var removeAkt = function(naslov) {
 						var req = {
-								method : 'DELETE',
-								url : 'http://localhost:8080/XML_Projekat/rest/services/odabraniAkti/remove/'+naslov
-							}
-							$http(req).then(function successCallback(response) {
-								$scope.listaOdabranihAkata = response.data;								
-							});
+							method : 'DELETE',
+							url : 'http://localhost:8080/XML_Projekat/rest/services/odabraniAkti/remove/'
+									+ naslov
+						}
+						$http(req).then(function successCallback(response) {
+							$scope.listaOdabranihAkata = response.data;
+						});
 					};
-					
-					
-					//INIT FUNKCIJE
+
+					// INIT FUNKCIJE
 					loadAkt();
 					loadOdabaniAkt();
-					
+					loadAmandman();
+					loadPredlozeniAmandman();
 					$scope.sadrzajAkta = "";
 					$scope.viewAkt = "";
 					// 'gen/html/zakon_o_izvrsenju_i_obezbedjenju.html'
@@ -90,18 +115,20 @@ var app = angular
 									+ username + '/' + password
 
 						};
-						$http(req).then(function successCallback(response) {
-							var dobijeno = response.data;
-							if (dobijeno != null) {
-								user = dobijeno.uloga;
-								if(user == 'gradjanin'){
-									$window.location.href = 'http://localhost:8080/XML_Projekat/#/gr';
-								}
-								if(user == 'predsednikVlade'){
-									$window.location.href = 'http://localhost:8080/XML_Projekat/#/vl';
-								}
-							}
-						});
+						$http(req)
+								.then(
+										function successCallback(response) {
+											var dobijeno = response.data;
+											if (dobijeno != null) {
+												user = dobijeno.uloga;
+												if (user == 'gradjanin') {
+													$window.location.href = 'http://localhost:8080/XML_Projekat/#/gr';
+												}
+												if (user == 'predsednikVlade') {
+													$window.location.href = 'http://localhost:8080/XML_Projekat/#/vl';
+												}
+											}
+										});
 					};
 
 					$scope.test = "";
@@ -127,127 +154,219 @@ var app = angular
 					 * 'amandmaniController' }).state('prVlad.glasanje', { url :
 					 * '/glasanje', templateUrl : 'glasanje.html', controller :
 					 * 'glasanjeController' });
-					
+					 * 
 					 */
-					$stateProvider.state('prVlad', {// naziv stanja! 
-						url : '/vl',
-						templateProvider : function(session, $stateParams, $templateFactory){
-							session.loadRole();      
-							if(session.role == 'predsednikVlade'){
-					                return $templateFactory.fromUrl('predsednikVlade.html', $stateParams);
-					              } else {
-					                return $templateFactory.fromUrl('nisteUlogovani.html', $stateParams);
-					              }
-					        }
-					}).state('prVlad.amandmani', { 
-						url : '/amandmani',
-						templateProvider : function(session, $stateParams, $templateFactory){
-							session.loadRole();  
-							if(session.role == 'predsednikVlade'){
-				                return $templateFactory.fromUrl('kreiranjeAmandmana2.html', $stateParams);
-				              } else {
-				                return $templateFactory.fromUrl('nisteUlogovani.html', $stateParams);
-				              }
-						},
-						controller : 'amandmaniController' 
-					}).state('prVlad.akt', { 
-						url : '/akt',
-						templateProvider : function(session, $stateParams, $templateFactory){
-							session.loadRole();  
-							if(session.role == 'predsednikVlade'){
-				                return $templateFactory.fromUrl('odabirAkata2.html', $stateParams);
-				              } else {
-				                return $templateFactory.fromUrl('nisteUlogovani.html', $stateParams);
-				              }
-						}
-					}).state('prVlad.glasanje', { 
-						url : '/glasanje',
-						templateProvider : function(session, $stateParams, $templateFactory){
-							session.loadRole();  
-							if(session.role == 'predsednikVlade'){
-								
-				                return $templateFactory.fromUrl('glasanje.html', $stateParams);
-				              } else {
-				                return $templateFactory.fromUrl('nisteUlogovani.html', $stateParams);
-				              }
-						},
-						controller: 'glasanjeController'
-					});
-					
-					$stateProvider.state('gradjanin',{// naziv stanja!
+					$stateProvider.state(
+							'prVlad',
+							{// naziv stanja!
+								url : '/vl',
+								templateProvider : function(session,
+										$stateParams, $templateFactory) {
+									session.loadRole();
+									if (session.role == 'predsednikVlade') {
+										return $templateFactory.fromUrl(
+												'predsednikVlade.html',
+												$stateParams);
+									} else {
+										return $templateFactory.fromUrl(
+												'nisteUlogovani.html',
+												$stateParams);
+									}
+								}
+							}).state(
+							'prVlad.amandmani',
+							{
+								url : '/amandmani',
+								templateProvider : function(session,
+										$stateParams, $templateFactory) {
+									session.loadRole();
+									if (session.role == 'predsednikVlade') {
+										return $templateFactory.fromUrl(
+												'kreiranjeAmandmana2.html',
+												$stateParams);
+									} else {
+										return $templateFactory.fromUrl(
+												'nisteUlogovani.html',
+												$stateParams);
+									}
+								},
+								controller : 'amandmaniController'
+							}).state(
+							'prVlad.akt',
+							{
+								url : '/akt',
+								templateProvider : function(session,
+										$stateParams, $templateFactory) {
+									session.loadRole();
+									if (session.role == 'predsednikVlade') {
+										return $templateFactory.fromUrl(
+												'odabirAkata2.html',
+												$stateParams);
+									} else {
+										return $templateFactory.fromUrl(
+												'nisteUlogovani.html',
+												$stateParams);
+									}
+								}
+							}).state(
+							'prVlad.glasanje',
+							{
+								url : '/glasanje',
+								templateProvider : function(session,
+										$stateParams, $templateFactory) {
+									session.loadRole();
+									if (session.role == 'predsednikVlade') {
+
+										return $templateFactory.fromUrl(
+												'glasanje.html', $stateParams);
+									} else {
+										return $templateFactory.fromUrl(
+												'nisteUlogovani.html',
+												$stateParams);
+									}
+								},
+								controller : 'glasanjeController'
+							});
+
+					$stateProvider.state('gradjanin', {// naziv stanja!
 						url : '/gr',
-						templateProvider : function(session, $stateParams, $templateFactory){
-					         	session.loadRole();	
-					              if(session.role == 'gradjanin'){
-					                return $templateFactory.fromUrl('gradjanin.html', $stateParams);
-					              } else {
-					                return $templateFactory.fromUrl('nisteUlogovani.html', $stateParams);
-					              }
-					        }
-					      });
-					
-}).service('session', function($timeout, $q) {
+						templateProvider : function(session, $stateParams,
+								$templateFactory) {
+							session.loadRole();
+							if (session.role == 'gradjanin') {
+								return $templateFactory.fromUrl(
+										'gradjanin.html', $stateParams);
+							} else {
+								return $templateFactory.fromUrl(
+										'nisteUlogovani.html', $stateParams);
+							}
+						}
+					});
+					$stateProvider.state('gradjanin.akti', {
+						url : '/akt',
+						templateProvider : function(session, $stateParams,
+								$templateFactory) {
+
+							session.loadRole();
+							console.log("uloga" + session.role);
+							if (session.role == 'gradjanin') {
+								return $templateFactory.fromUrl(
+										'pregledAkata.html', $stateParams);
+							} else {
+								return $templateFactory.fromUrl(
+										'nisteUlogovani.html', $stateParams);
+							}
+						}
+					});
+					$stateProvider.state('gradjanin.amandmani', {
+						url : '/amandman',
+						templateProvider : function(session, $stateParams,
+								$templateFactory) {
+							session.loadRole();
+							if (session.role == 'gradjanin') {
+								return $templateFactory.fromUrl(
+										'pregledAmandmana.html', $stateParams);
+							} else {
+								return $templateFactory.fromUrl(
+										'nisteUlogovani.html', $stateParams);
+							}
+						}
+					});
+
+				})
+		.service('session', function($timeout, $q) {
 			this.role = "";
 
 			this.loadRole = function() {
-				console.log("radi loadRole "+user);
+				console.log("radi loadRole " + user);
 				this.role = user;
 				return this.role;
 			};
-		}).controller('amandmaniController', function($scope, $http) {
+		})
+		.controller(
+				'amandmaniController',
+				function($scope, $http) {
 
-			var addAmandman = function(naslovAkta,naslovAmandmana,content){
-				var req = {
-						method : 'PUT',
-						url : 'http://localhost:8080/XML_Projekat/rest/services/amandman/add/'+naslovAkta+'/'+naslovAmandmana+'/'+content
-					}
-					$http(req).then(function successCallback(response) {
-						$scope.$parent.listaAmandmana = response.data;								
-					});
-			};
-			
-			
-			$scope.selectAkt = function(akt) {
-				$scope.$parent.selectedAkt = akt;
-
-			};
-
-			$scope.createAmandman = function(akt,name, content) {
-				addAmandman(akt, name, content);
-
-			};
-
-			$scope.selectAmandman = function(amandman) {
-				$scope.$parent.selectedAmandman = amandman;
-			};
-			$scope.test = "";
-
-		}).controller('glasanjeController', function($scope,$http) {
-			
-			var removeAmandman = function(naslovAmandmana){
-				var req = {
-						method : 'DELETE',
-						url : 'http://localhost:8080/XML_Projekat/rest/services/amandman/add/'+naslovAmandmana
-					}
-					$http(req).then(function successCallback(response) {
-						$scope.$parent.listaAmandmana = response.data;								
-					});
-			};
-			
-			
-			$scope.selectAmandman = function(amandman) {
-				$scope.$parent.selectedAmandman = amandman;
-			};
-			
-			$scope.izvediGlasanje = function(amandman,za,protiv,uzdrzani){
-			
-				var req = {
-						method : 'GET',
-						url : 'http://localhost:8080/XML_Projekat/rest/services/vote/'+za+'/'+protiv+'/'+uzdrzani+'/'+amandman
+					var addAmandman = function(naslovAkta, naslovAmandmana,
+							content) {
+						var req = {
+							method : 'PUT',
+							url : 'http://localhost:8080/XML_Projekat/rest/services/amandman/add/'
+									+ naslovAkta
+									+ '/'
+									+ naslovAmandmana
+									+ '/'
+									+ content
+						}
+						$http(req).then(function successCallback(response) {
+							$scope.$parent.listaAmandmana = response.data;
+						});
 					};
-					$http(req).then(function successCallback(response) {
-						$scope.$parent.listaAmandmana = response.data;
-						$scope.$parent.selectedAmandman = "";
-					});
-			}
-		});
+
+					$scope.selectAkt = function(akt) {
+						$scope.$parent.selectedAkt = akt;
+
+					};
+
+					$scope.createAmandman = function(akt, name, content) {
+						addAmandman(akt, name, content);
+
+					};
+
+					$scope.selectAmandman = function(amandman) {
+						$scope.$parent.selectedAmandman = amandman;
+					};
+					$scope.test = "";
+
+				})
+		.controller(
+				'glasanjeController',
+				function($scope, $http) {
+
+					var loadAmandman = function() {
+						var req = {
+							method : 'GET',
+							url : 'http://localhost:8080/XML_Projekat/rest/services/amandmani'
+						}
+						$http(req).then(
+								function successCallback(response) {
+									$scope.$parent.listaIzglasanihAmandmana = response.data;
+								});
+					};
+
+					var removeAmandman = function(naslovAmandmana) {
+						var req = {
+							method : 'DELETE',
+							url : 'http://localhost:8080/XML_Projekat/rest/services/amandman/add/'
+									+ naslovAmandmana
+						}
+						$http(req).then(function successCallback(response) {
+							$scope.$parent.listaAmandmana = response.data;
+						});
+					};
+
+					$scope.selectAmandman = function(amandman) {
+						$scope.$parent.selectedAmandman = amandman;
+					};
+
+					$scope.izvediGlasanje = function(amandman, za, protiv,
+							uzdrzani) {
+
+						var req = {
+							method : 'GET',
+							url : 'http://localhost:8080/XML_Projekat/rest/services/vote/'
+									+ za
+									+ '/'
+									+ protiv
+									+ '/'
+									+ uzdrzani
+									+ '/'
+									+ amandman
+						};
+						$http(req).then(function successCallback(response) {
+							$scope.$parent.listaAmandmana = response.data;
+							$scope.$parent.selectedAmandman = "";
+							loadAmandman();
+						});
+					}
+				});
